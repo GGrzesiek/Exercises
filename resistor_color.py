165678f4e39dacaf4e34c0caf4bea126ee5af90a
Grzegorz Kryszczuk <kryszczuk.grzegorz_4a@op.pl>
2024-05-28 14:05:51 +0200

    Create resistor_color.py

---
+++ resistor_color.py
@@ -0,0 +1,16 @@
+def color_code(color):
+    return colors().index(color)
+
+
+def colors():
+    colors_list = ["black",
+          "brown",
+          "red",
+          "orange",
+          "yellow",
+          "green",
+          "blue",
+          "violet",
+          "grey",
+          "white"]
+    return colors_list
