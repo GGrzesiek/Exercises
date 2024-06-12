6232f06110489307b2c41003048375d6e4d5084b
Grzegorz Kryszczuk <kryszczuk.grzegorz_4a@op.pl>
2024-06-13 00:51:43 +0200

    added binary_search and started function square_root

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
-import rna_transcription as rna
-print(rna.to_rna('GCTA'))
+import binary_search as binary
+print(binary.find([1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 634], 144))
---
+++ square_root.py
@@ -0,0 +1,9 @@
+def square_root(number):
+    """Calculate the square root of a number without build-in functions.
+
+    :param number: int - number to calculate the square root of.
+    :return: float - square root of the provided number.
+    """
+    digits = map(int, str(number))
+    
+
